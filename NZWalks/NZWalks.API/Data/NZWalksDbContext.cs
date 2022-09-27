85a649f838f079b63b1cd491d2a173242e02a1d4
shobhit <shobhit.fullstack@gmail.com>
2022-09-27 23:33:35 +0530

    End of Section 2

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    // always inherits from the base class and takes the namespace "Microsoft.EntityFrameworkCore"
+    // short cut for adding namespace ctrl + . (dot)
+    public class NZWalksDbContext: DbContext  
+    {
+        // creating constructor using short cut ctor tab to pass the dbcontext options which takes the class 
+        // and base options in the last after the colon
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        {
+
+        }
+
+        // now creating properties of DB set types..
+        // With this DBSet will be telling the entity framework to create table for us in database if doesn't exists...
+        public DbSet<Region> Regions { get; set; } 
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalksDifficulty { get; set; }
+
+    }
+}
---
+++ NZWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+
+        public double Lat { get; set; }
+
+        public double Long { get; set; }
+
+        public long Population { get; set;  }
+
+        // navigation properties
+
+        public IEnumerable<Walk> Walks { get; set; }
+
+    }
+}
