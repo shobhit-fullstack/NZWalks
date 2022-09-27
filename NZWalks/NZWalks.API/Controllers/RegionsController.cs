4a5138ce1379f58d6be5b4b36926e6d3147a3b41
shobhit <shobhit.fullstack@gmail.com>
2022-09-28 01:01:34 +0530

    End of Section 3

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("Regions")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+
+        public IMapper Mapper { get; }
+
+        [HttpGet]
+        //public IActionResult GetAllRegions()
+        //{
+        //    var regions = new List<Region>()
+        //    { 
+        //        new Region
+        //        { 
+        //            Id = Guid.NewGuid(),
+        //            Name = "Wellington",
+        //            Code = "WLG",
+        //            Area = 227755,
+        //            Lat = -1.8822,
+        //            Long = 299.88,
+        //            Population = 500000
+        //        },
+        //        new Region
+        //        {
+        //            Id = Guid.NewGuid(),
+        //            Name = "Auckland",
+        //            Code = "AUCK",
+        //            Area = 227755,
+        //            Lat = -1.8822,
+        //            Long = 299.88,
+        //            Population = 500000
+        //        },
+        //    };
+
+        //    return Ok(regions);
+
+        //}
+        public async  Task<IActionResult> GetAllRegions()
+        {
+            var regions = await regionRepository.GetAll();
+            var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions);
+            return Ok(regionsDTO);
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Repositories/IRegionRepository.cs
@@ -0,0 +1,10 @@
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public interface IRegionRepository
+    {
+        //Making in Asynchronous by adding Task in the starting of the IEnumerable...
+       Task<IEnumerable<Region>> GetAll();
+    }
+}
